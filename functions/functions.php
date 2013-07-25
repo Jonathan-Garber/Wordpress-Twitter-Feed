e9311001e12740dd85320dba93d1f6621646b686
Jonathan-Garber <jonathan@techstudio.co>
2013-07-24 22:26:44 -0400

    Fixed typo in functions causing 500 error

--- functions/functions.php
+++ functions/functions.php
@@ -5,17 +5,17 @@ function twitter_feed_git_init() {
 	require_once 'git/updater.php';
 	if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
 	    $config = array(
-	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-	        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-	        'api_url' => 'https://api.github.com/repos/Jonathan-Garber/Wordpress-Twitter-Feed', // the github API url of your github repo
-	        'raw_url' => 'https://raw.github.com/Jonathan-Garber/Wordpress-Twitter-Feed/master', // the github raw url of your github repo
-	        'github_url' => 'https://github.com/Jonathan-Garber/Wordpress-Twitter-Feed', // the github url of your github repo
-	        'zip_url' => 'https://github.com/Jonathan-Garber/Wordpress-Twitter-Feed/zipball/master', // the zip url of the github repo
-	        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-	        'requires' => '3.0', // which version of WordPress does your plugin require?
-	        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-	        'readme' => 'README.md', // which file to use as the readme for the version number
-	        'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
+	        'slug' => plugin_basename(__FILE__),
+	        'proper_folder_name' => 'Wordpress-Twitter-Feed',
+	        'api_url' => 'https://api.github.com/repos/Jonathan-Garber/Wordpress-Twitter-Feed',
+	        'raw_url' => 'https://raw.github.com/Jonathan-Garber/Wordpress-Twitter-Feed/master',
+	        'github_url' => 'https://github.com/Jonathan-Garber/Wordpress-Twitter-Feed',
+	        'zip_url' => 'https://github.com/Jonathan-Garber/Wordpress-Twitter-Feed/zipball/master',
+	        'sslverify' => true,
+	        'requires' => '3.5.0',
+	        'tested' => '3.5.0',
+	        'readme' => 'README.md',
+	        'access_token' => '',
 	    );
 	    new WP_GitHub_Updater($config);
 	}
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Jonathan-Garber
 Tags: Wordpress, Twitter, Statuses, Feed
 Requires at least: 3.5.1
 Tested up to: 3.5.2
-Stable tag: 0.0.1
+Stable tag: 0.0.2
 License: GPLv2 or later
 License URL: License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
